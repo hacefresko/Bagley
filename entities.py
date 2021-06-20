56a4f1eaac68d33c73744816b037850c6502a677
hacefresko <hacefresko@gmail.com>
2021-06-20 13:16:22 +0200

    Reffactored POST data storing

--- entities.py
+++ entities.py
@@ -610,24 +610,28 @@ class Utils:
 
     # Substitutes all values whose keys match with match parameter to newValue. If match is None, it will substitute all values.
     # It uses content_type header to know what type of POST data is, so it can be more precise
-    # For multipart/form-data, it also changes the boundary to boundary_substitute the first time it is applied
+    # For multipart/form-data, it also substitutes the boundary since its value is usually random/partially random
     # If an exception occurs, a copy of the original data is returned
     # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Type
     @staticmethod
     def substitutePOSTData(content_type, data, match, newValue):
         boundary_substitute = 'BOUNDARY'
+        
         if not content_type:
             try:
                 return json.dumps(Utils.replaceJSON(json.loads(data), match, newValue))
             except:
                 return  Utils.replaceURLencoded(data, match, newValue)
+
         try:
             if 'multipart/form-data' in content_type:
+
                 # If data has already been parsed so boundary has changed
                 if '--'+boundary_substitute in data:
                     boundary = '--'+boundary_substitute
                 else:
                     boundary = '--' + content_type.split('; ')[1].split('=')[1]
+
                 new_data = '--'+boundary_substitute
                 for fragment in data.split(boundary)[1:-1]:
                     name = fragment.split('\r\n')[1].split('; ')[1].split('=')[1].strip('"')
@@ -644,12 +648,16 @@ class Utils:
                         else:
                             new_content = content
                     
-                    new_data += fragment.split('name="')[0] + 'name="' + name + '"; ' + "; ".join(fragment.split('\r\n')[1].split('; ')[2:]) + '\r\n' + new_content + '\r\n--'+boundary_substitute
+                    new_data += fragment.split('name="')[0] + 'name="' + name + '"; ' + "; ".join(fragment.split('\r\n')[1].split('; ')[2:]) + '\r\n\r\n' + new_content + '\r\n--'+boundary_substitute
                 new_data += '--'
+
                 return new_data
+
             elif 'application/json' in content_type:
                 return json.dumps(Utils.replaceJSON(json.loads(data), match, newValue))
+
             elif 'application/x-www-form-urlencoded' in content_type:
                 return Utils.replaceURLencoded(data, match, newValue)
+
         except Exception as e:
             return data
