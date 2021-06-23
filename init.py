4cef93e0d67aca41cf520989278405f03d03c5c0
hacefresko <hacefresko@gmail.com>
2021-06-23 02:04:56 +0200

    Forgot to restore when debugging

--- init.py
+++ init.py
@@ -10,25 +10,23 @@ def sigint_handler(sig, frame):
     quit()
 signal.signal(signal.SIGINT, sigint_handler)
 
-#try:
-#    opts, args = getopt.getopt(sys.argv[1:], 'D:')
-#
-#    for opt, arg in opts:
-#        if opt == '-D':
-#            scope_file_name = arg
-#            scope_found = True
-#
-#    if not scope_found or scope_file_name == '':
-#        raise Exception
-#            
-#except Exception:
-#    print(os.path.basename(__file__) + ' -D <scope file>')
-#    exit()
-#
-#print("[+] Starting time: %s" % datetime.datetime.now())
-#print("[+] Scope file: %s" % scope_file_name)
+try:
+    opts, args = getopt.getopt(sys.argv[1:], 'D:')
 
-scope_file_name = "domains.txt"
+    for opt, arg in opts:
+        if opt == '-D':
+            scope_file_name = arg
+            scope_found = True
+
+    if not scope_found or scope_file_name == '':
+        raise Exception
+            
+except Exception:
+    print(os.path.basename(__file__) + ' -D <scope file>')
+    exit()
+
+print("[+] Starting time: %s" % datetime.datetime.now())
+print("[+] Scope file: %s" % scope_file_name)
 
 try:
     scope_file = open(scope_file_name, 'r')
