6a933376c7e7a96d3aebb5a058032f7f23b9d57b
hacefresko <hacefresko@gmail.com>
2021-07-13 04:03:27 +0200

    Added locks to db

--- lib/database.py
+++ lib/database.py
@@ -5,6 +5,7 @@ DB_NAME = 'bagley.db'
 class DB:
     # Dict of tids and their instances
     __instance = None
+    __lock = threading.Lock()
 
     def __new__(cls):
         if DB.__instance is None:
@@ -15,9 +16,12 @@ class DB:
         self.__connection = sqlite3.connect(DB_NAME, check_same_thread=False)
 
     def query(self, query, params):
+        DB.__lock.acquire()
         cursor = self.__connection.cursor()
         cursor.execute(query, params)
         self.__connection.commit()
+        DB.__lock.release()
+
         return cursor
 
     def close(self):
