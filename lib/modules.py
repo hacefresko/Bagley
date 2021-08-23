5352bc97980c4169b4bf75be011159337156866c
hacefresko <hacefresko@gmail.com>
2021-08-23 14:14:23 +0200

    Fixing bugs

--- lib/modules.py
+++ lib/modules.py
@@ -108,9 +108,8 @@ class Crawler (threading.Thread):
 
         # Third argument from open() means to request synchronously so selenium wait for it to be completed
         self.driver.execute_script("""
-        function post(path, data, headers, timeout) {
+        function post(path, data, headers) {
             var xhr = new XMLHttpRequest();
-            xhr.timeout = timeout;
             xhr.open("POST", path, false);
 
             Object.keys(headers).forEach(function(key){
@@ -120,8 +119,8 @@ class Crawler (threading.Thread):
             xhr.send(data)
         }
         
-        post(arguments[0], arguments[1], arguments[2], arguments[3]);
-        """, path, data, headers_dict, lib.config.TIMEOUT)
+        post(arguments[0], arguments[1], arguments[2]);
+        """, path, data, headers_dict)
 
     # Inserts in the database the request and its response if url belongs to the scope. 
     # Returns (request, response). Headers and cookies params are extra headers and cookies added to the request
