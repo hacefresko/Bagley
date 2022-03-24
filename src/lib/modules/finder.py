ab0faf4bde2e1e829815cca945d59045ae422786
Hacefresko <victorfrespe@gmail.com>
2022-03-24 13:36:00 +0100

    bugs

--- src/lib/modules/finder.py
+++ src/lib/modules/finder.py
@@ -121,6 +121,7 @@ class Finder(Module):
         # Collect errors if execution fails
         if process.poll() != 0:
             error = process.stderr.readline().decode('utf-8', errors='ignore')
+            lib.controller.Controller.send_msg(error, "finder")
             # If errorcodes must be specified to gobuster
             if 'Error: the server returns a status code that matches the provided options for non existing urls' in error:
                 try:
@@ -133,30 +134,22 @@ class Finder(Module):
 
     def __findSubDomains(self,domain):
         # Rate is limited to 1 always because if doesn't, it adds too much traffic and ISPs are very greedy
-        command = [shutil.which('subfinder'), '-oJ', '-nC', '-silent', '-all', '-d', str(domain)[1:], '-rate-limit', '1']
+        command = [shutil.which('subfinder'), '-oJ', '-nc', '-all', '-d', str(domain)[1:], '-rl', '1']
 
         lib.controller.Controller.send_msg("Finding subdomains for %s" % str(domain)[1:], "finder")
 
-        process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        
-        line = process.stdout.readline().decode('utf-8', errors='ignore')
-        while line:
-            try:
-                discovered = json.loads(line).get('host')
-                # Sometimes output frome some sources is Domain\nDomain\nDomain...
-                for d in discovered.split('\n'):
-                    try:
-                        # Check if domain really exist, since subfinder does not check it
-                        socket.gethostbyname(d)
-                        if Domain.checkScope(d) and not Domain.get(discovered):
-                            lib.controller.Controller.send_msg("DOMAIN FOUND: Inserted %s to database" % d, "finder")
-                            Domain.insert(d)
-                    except:
-                        continue
-            except:
-                pass
-            finally:
-                line = process.stdout.readline().decode('utf-8', errors='ignore')
+        for line in subprocess.run(command, capture_output=True, encoding='utf-8', input=str(domain)).stdout.splitlines():
+            discovered = json.loads(line).get('host')
+            # Sometimes output from some sources is Domain\nDomain\nDomain...
+            for d in discovered.split('\n'):
+                try:
+                    # Check if domain really exist, since subfinder does not check it
+                    socket.gethostbyname(d)
+                    if Domain.checkScope(d) and not Domain.get(discovered):
+                        lib.controller.Controller.send_msg("DOMAIN FOUND: Inserted %s to database" % d, "finder")
+                        Domain.insert(d)
+                except:
+                    continue
         
     def run(self):
         directories = Path.yieldDirectories()
