09091db6c91077aeff1014c7a6e2cbdae8f886d1
hacefresko <victorfrespe@gmail.com>
2022-04-27 23:49:54 +0200

    Added more keys to searchKeys

--- src/lib/modules/static_analyzer.py
+++ src/lib/modules/static_analyzer.py
@@ -10,40 +10,65 @@ class Static_Analyzer (Module):
         super().__init__(['linkfinder', 'unwebpack_sourcemap', 'codeql'], stop)
         self.crawler = crawler
 
-    def __searchKeys(self, element):
-        # https://github.com/m4ll0k/SecretFinder pattern list + https://github.com/hahwul/dalfox greeping list
+    def _searchKeys(self, element):
+        # Taken from https://github.com/sdushantha/dora/blob/main/dora/db/data.json
         patterns = {
-            'rsa-key':                          r'-----BEGIN RSA PRIVATE KEY-----|-----END RSA PRIVATE KEY-----',
-            'priv-key':                         r'-----BEGIN PRIVATE KEY-----|-----END PRIVATE KEY-----',
-            'ssh_dsa_private_key' :             r'-----BEGIN DSA PRIVATE KEY-----',
-            'ssh_dc_private_key' :              r'-----BEGIN EC PRIVATE KEY-----',
-            'pgp_private_block' :               r'-----BEGIN PGP PRIVATE KEY BLOCK-----',
-            'aws-s3':                           r's3\\.amazonaws.com[/]+|[a-zA-Z0-9_-]*\\.s3\\.amazonaws.com',
-            'aws-appsync-graphql':              r'da2-[a-z0-9]{26}',
-            'slack-webhook1':                   r'https://hooks.slack.com/services/T[a-zA-Z0-9_]{8}/B[a-zA-Z0-9_]{8}/[a-zA-Z0-9_]{24}',
-            'slack-webhook2':                   r'https://hooks.slack.com/services/T[a-zA-Z0-9_]{8,10}/B[a-zA-Z0-9_]{8,10}/[a-zA-Z0-9_]{24}',
-            'slack-token':                      r'(xox[p|b|o|a]-[0-9]{12}-[0-9]{12}-[0-9]{12}-[a-z0-9]{32})',
-            'facebook-oauth':                   r"[f|F][a|A][c|C][e|E][b|B][o|O][o|O][k|K].{0,30}['\"\\s][0-9a-f]{32}['\"\\s]",
-            'twitter-oauth':                    r"[t|T][w|W][i|I][t|T][t|T][e|E][r|R].{0,30}['\"\\s][0-9a-zA-Z]{35,44}['\"\\s]",
-            'heroku-api':                       r'[h|H][e|E][r|R][o|O][k|K][u|U].{0,30}[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{12}',
-            'mailgun-api':                      r'key-[0-9a-zA-Z]{32}',
-            'mailchamp-api':                    r'[0-9a-f]{32}-us[0-9]{1,2}',
-            'picatic-api':                      r'sk_live_[0-9a-z]{32}',
-            'google-oauth':                     r'ya29\\.[0-9A-Za-z\\-_]+',
-            'aws-access-key':                   r'AKIA[0-9A-Z]{16}',
-            'amazon-mws-auth-token':            r'amzn\\.mws\\.[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}',
-            'facebook-access-token':            r'EAACEdEose0cBA[0-9A-Za-z]+',
-            'github-access-token':              r'[a-zA-Z0-9_-]*:[a-zA-Z0-9_\\-]+@github\\.com*',
-            'azure-storage':                    r'[a-zA-Z0-9_-]*\\.file.core.windows.net',
-            'telegram-bot-api-key':             r'[0-9]+:AA[0-9A-Za-z\\-_]{33}',
-            'square-access-token':              r'sq0atp-[0-9A-Za-z\\-_]{22}',
-            'square-oauth-secret':              r'sq0csp-[0-9A-Za-z\\-_]{43}',
-            'twilio-api-key':                   r'SK[0-9a-fA-F]{32}',
-            'braintree-token':                  r'access_token\\$production\\$[0-9a-z]{16}\\$[0-9a-f]{32}',
-            'stripe-api-key':                   r'sk_live_[0-9a-zA-Z]{24}',
-            'stripe-restricted-key':            r'rk_live_[0-9a-zA-Z]{24}',
-            'json_web_token' :                  r'ey[A-Za-z0-9-_=]+\.[A-Za-z0-9-_=]+\.?[A-Za-z0-9-_.+/=]*$',
-            'paypal_braintree_access_token' :   r'access_token\$production\$[0-9a-z]{16}\$[0-9a-f]{32}'
+            "Google API Key": "AIza[0-9A-Za-z-_]{35}",
+            "Mailgun Private Key": "key-[0-9a-zA-Z]{32}",
+            "Heroku API key": "[h|H][e|E][r|R][o|O][k|K][u|U].{0,30}[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{12}",
+            "Slack API token": "(xox[p|b|o|a]-[0-9]{12}-[0-9]{12}-[0-9]{12}-[a-z0-9]{32})",
+            "Slack Webhook": "https://hooks.slack.com/services/T[a-zA-Z0-9_]{8}/B[a-zA-Z0-9_]{8}/[a-zA-Z0-9_]{24}",
+            "MailChimp API Key": "[0-9a-f]{32}-us[0-9]{1,2}",
+            "Facebook Access Token": "EAACEdEose0cBA[0-9A-Za-z]+",
+            "Facebook Secret Key": "(?i)(facebook|fb)(.{0,20})?(?-i)['\\\"][0-9a-f]{32}['\\\"]",
+            "Facebook Client ID": "(?i)(facebook|fb)(.{0,20})?['\\\"][0-9]{13,17}['\\\"]",
+            "Twitter Secret Key": "(?i)twitter(.{0,20})?['\\\"][0-9a-z]{35,44}['\\\"]",
+            "Twitter Client ID": "(?i)twitter(.{0,20})?['\\\"][0-9a-z]{18,25}['\\\"]",
+            "Github Personal Access Token": "ghp_[0-9a-zA-Z]{36}",
+            "Github OAuth Access Token": "gho_[0-9a-zA-Z]{36}",
+            "Github App Token": "(ghu|ghs)_[0-9a-zA-Z]{36}",
+            "Github Refresh Token": "ghr_[0-9a-zA-Z]{76}",
+            "LinkedIn Client ID": "(?i)linkedin(.{0,20})?(?-i)[0-9a-z]{12}",
+            "LinkedIn Secret Key": "(?i)linkedin(.{0,20})?[0-9a-z]{16}",
+            "GitHub Access Token": "[a-zA-Z0-9_-]*:[a-zA-Z0-9_-]+@github\\.com*",
+            "Stripe Restricted API Token": "rk_live_[0-9a-zA-Z]{24}",
+            "Stripe Standard API Token": "sk_live_[0-9a-zA-Z]{24}",
+            "Square Access Token": "sqOatp-[0-9A-Za-z\\-_]{22}",
+            "Square OAuth Secret": "sq0csp-[ 0-9A-Za-z\\-_]{43}",
+            "PayPal/Braintree Access Token": "access_token\\$production\\$[0-9a-z]{16}\\$[0-9a-f]{32}",
+            "Amazon MWS Auth Token": "amzn\\.mws\\.[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}",
+            "Picatic API Key": "sk_[live|test]_[0-9a-z]{32}",
+            "Google OAuth Access Key": "ya29\\.[0-9A-Za-z\\-_]+",
+            "Google OAuth ID": "[0-9(+-[0-9A-Za-z_]{32}.apps.googleusercontent.com",
+            "StackHawk API Key": "hawk\\.[0-9A-Za-z\\-_]{20}\\.[0-9A-Za-z\\-_]{20}",
+            "NuGet API Key": "oy2[a-z0-9]{43}",
+            "SendGrid Token": "SG\\.[0-9A-Za-z\\-_]{22}\\.[0-9A-Za-z-_]{43}",
+            "AWS Access Key": "(A3T[A-Z0-9]|AKIA|AGPA|AROA|AIPA|ANPA|ANVA|ASIA)[A-Z0-9]{16}",
+            "AWS Secret Key": "(?i)aws(.{0,20})?(?-i)['\\\"][0-9a-zA-Z/+]{40}['\\\"]",
+            "Google Cloud Platfor API Key": "[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}",
+            "Zoho Webhook Token": "https://creator\\.zoho\\.com/api/[A-Za-z0-9/\\-_\\.]+\\?authtoken=[A-Za-z0-9]+",
+            "Zapier Webhook": "https://(?:www.)?hooks\\.zapier\\.com/hooks/catch/[A-Za-z0-9]+/[A-Za-z0-9]+/",
+            "New Relic Admin API Key": "NRAA-[a-f0-9]{27}",
+            "New Relic Insights Key": "NRI(?:I|Q)-[A-Za-z0-9\\-_]{32}",
+            "New Relic REST API Key": "NRRA-[a-f0-9]{42}",
+            "New Relic Synthetics Location Key": "NRSP-[a-z]{2}[0-9]{2}[a-f0-9]{31}",
+            "Microsoft Teams Webhook": "https://outlook\\.office\\.com/webhook/[A-Za-z0-9\\-@]+/IncomingWebhook/[A-Za-z0-9\\-]+/[A-Za-z0-9\\-]+",
+            "Google FCM Server Key": "AAAA[a-zA-Z0-9_-]{7}:[a-zA-Z0-9_-]{140}",
+            "Google Calendar URI": "https://www\\.google\\.com/calendar/embed\\?src=[A-Za-z0-9%@&;=\\-_\\./]+",
+            "Discord Webhook": "https://discordapp\\.com/api/webhooks/[0-9]+/[A-Za-z0-9-_]+",
+            "Cloudinary Credentials": "cloudinary://[0-9]+:[A-Za-z0-9-_.]+@[A-Za-z0-9-_.]+",
+            "Bitly Secret Key": "R_[0-9a-f]{32}",
+            "Amazon SNS Topic": "arn:aws:sns:[a-z0-9-]+:[0-9]+:[A-Za-z0-9-_]+",
+            "PyPI Upload Token": "pypi-AgEIcHlwaS5vcmc[A-Za-z0-9-_]{50,1000}",
+            "Shopify Private App Access Token": "shppa_[a-fA-F0-9]{32}",
+            "Shopify Custom App Access Token": "shpca_[a-fA-F0-9]{32}",
+            "Shopify Access Token": "shpat_[a-fA-F0-9]{32}",
+            "Shopify Shared Secret": "shpss_[a-fA-F0-9]{32}",
+            "Dynatrace Token": "dt0[a-zA-Z]{1}[0-9]{2}\\.[A-Z0-9]{24}\\.[A-Z0-9]{64}",
+            "Twilio API Key": "(?i)twilio(.{0,20})?SK[0-9a-f]{32}",
+            "MongoDB Cloud Connection String": "mongodb\\+srv://(.*)",
+            "Riot Games Developer API Key": "RGAPI-[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}",
+            "SerpAPI": "[a-f0-9]{64}"
         }
 
         if isinstance(element, Script):
@@ -195,11 +220,11 @@ class Static_Analyzer (Module):
 
                     # Scripts embedded in html file are analyzed with the whole response body
                     if len(script.getPaths()) > 0:
-                        self.__searchKeys(script)
+                        self._searchKeys(script)
                 else:
                     response = next(responses)
                     if response and response.body and response.code == 200:
-                        self.__searchKeys(response)
+                        self._searchKeys(response)
                     else:
                         time.sleep(5)
                         continue
