3badf8d6f6724538fd9f9cd84587511dae350daf
hacefresko <victorfrespe@gmail.com>
2022-05-11 20:55:10 +0200

    Fixed config (it was debugging config)

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 import os
 
 # Log file
-LOG_FILE = 'bagley.log'
+LOG_FILE = '/var/log/bagley.log'
 
 # Database variables
 DB_USER = os.getenv("DB_USER") if os.getenv("DB_USER") else 'bagley'
@@ -34,13 +34,13 @@ PARAMS_BLACKLIST = ['csrf']
 HEADERS_BLACKLIST = ['date', 'cookie', 'set-cookie', 'content-length']
 
 # Absolute path for screenshot folder
-SCREENSHOT_FOLDER = 'screenshots/'
+SCREENSHOT_FOLDER = '/tmp/screenshots/'
 
 # Absolute path for scripts folder
-SCRIPTS_FOLDER = 'scripts/'
+SCRIPTS_FOLDER = '/tmp/scripts/'
 
 # Absolute path for rempotal files
-FILES_FOLDER = 'files/'
+FILES_FOLDER = '/tmp/'
 
 # Directory fuzzing wordlist
 DIR_FUZZING = '/usr/lib/SecLists/Discovery/Web-Content/common.txt'
@@ -49,4 +49,4 @@ DIR_FUZZING = '/usr/lib/SecLists/Discovery/Web-Content/common.txt'
 DOMAIN_FUZZING = '/usr/lib/SecLists/Discovery/DNS/subdomains-top1million-5000.txt'
 
 # Suite for codeql
-CODEQL_SUITE = '/home/hacefresko/Security/Software/Bagley/src/lib/modules/config/bagley_codeql.qls'
+CODEQL_SUITE = '/root/bagley/lib/modules/config/bagley_codeql.qls'
